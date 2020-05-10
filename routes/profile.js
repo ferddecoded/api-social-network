00579348057563701c55599c84c0a4ef0d04c03a
Ferdinand Ismael <ferdinandismael@ferdinands-mbp.lan>
2020-05-10 09:34:24 -0400

    updated profile route

--- routes/profile.js
+++ routes/profile.js
@@ -21,7 +21,7 @@ router.get('/me', auth, async (req, res) => {
       .populate('user', ['name', 'avatar']);
 
     if (!profile) {
-      res.status(400).json({ msg: 'There is no profile for this user' });
+      return res.status(400).json({ msg: 'There is no profile for this user' });
     }
 
     res.json(profile);
