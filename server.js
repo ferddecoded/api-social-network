c8f5f17f3f03537e87b39f786cafeaa712c315b8
Ferdinand Ismael <ferdinandismael@ferdinands-mbp.lan>
2020-05-11 13:30:54 -0400

    update express.static

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
 
-require('dotenv').config();
-
 const app = express();
 
 // Connect Database
@@ -22,7 +20,7 @@ app.use('/api/posts', require('./routes/posts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder name
-  app.use(express.static.apply('client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
