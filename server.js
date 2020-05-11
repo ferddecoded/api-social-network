e7989b96805205cb917edd342a27fcf63bbae6b1
Ferdinand Ismael <ferdinandismael@ferdinands-mbp.lan>
2020-05-11 12:28:05 -0400

    updated server for heroku deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -12,13 +13,20 @@ connectDB();
 // this will allow us to get the data in req.body
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/profile', require('./routes/profile'));
 app.use('/api/posts', require('./routes/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder name
+  app.use(express.static.apply('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
